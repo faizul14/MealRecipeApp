65180418ac913e7f24baebf2db3e0d96c9f5468e
Faezol <fpcyber61@gmail.com>
2024-01-18 18:12:12 +0800

    feat (HomeReceipe - ui): design layout feature  HomeReceipe

--- app/src/main/java/com/faezolmp/mealrecipeapp/presentation/detailReceipe/DetailReceipeViewModel.kt
+++ app/src/main/java/com/faezolmp/mealrecipeapp/presentation/detailReceipe/DetailReceipeViewModel.kt
@@ -3,6 +3,6 @@ package com.faezolmp.mealrecipeapp.presentation.detailReceipe
 import androidx.lifecycle.ViewModel
 import com.faezolmp.mealrecipeapp.core.domain.usecase.UseCase
 
-class DetailReceipeViewModel(val useCase: UseCase): ViewModel() {
+class DetailReceipeViewModel(useCase: UseCase): ViewModel() {
     val getsampleData = useCase.sampleinterface()
 }
